72fdb070078b3b3cb507ffe8aaaf7880923f6559
mokoko <guswlso2@naver.com>
2024-06-20 00:18:26 +0900

    todos 수정 api 추가

--- routes/todos.js
+++ routes/todos.js
@@ -29,6 +29,27 @@ router.post('/', auth, async (req, res) => {
   }
 });
 
+router.patch('/:id', auth, async (req, res) => {
+  try {
+    const { text } = req.body;
+    const todo = await Todo.findOneAndUpdate(
+      {
+        _id: req.params.id,
+        userId: req.user.userId,
+      },
+      { text },
+      { new: true }
+    );
+    if (!todo) {
+      return res.status(404).send({ message: 'Todo not found' });
+    }
+    res.json(todo);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: 'Server error' });
+  }
+});
+
 router.patch('/:id/toggle', auth, async (req, res) => {
   try {
     const todo = await Todo.findOne({
