aec322f822291d3e580f280e4016aaaf3d15c82d
mokoko <guswlso2@naver.com>
2024-06-21 17:28:13 +0900

    fix: server 오타

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI;
 
-pp.use(
+app.use(
   cors({
     origin: ['https://todolist-front-opal.vercel.app', 'http://localhost:5173'], // 허용할 도메인들
     credentials: true, // 쿠키를 포함한 요청을 허용
